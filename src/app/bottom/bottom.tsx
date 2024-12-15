6bf363fe3342c8a014f8777ee3167bde37b64042
trannam <tranhoainamnb1312@gmail.com>
2024-12-15 16:39:20 +0700

    aa

---
+++ src/app/bottom/bottom.tsx
@@ -0,0 +1,66 @@
+export default function bottom(){
+    return(
+        <footer className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
+  <div className="max-w-7xl mx-auto spx-4 sm:px-6 lg:px-8 py-10 grid grid-cols-1 md:grid-cols-4 gap-8 bg-gray-800  ">
+    <div>
+      <div className="flex items-center space-x-3">
+    
+        <span className="text-xl font-bold">Niolic Limited</span>
+      </div>
+      <p className="mt-4 text-sm">
+        Our company provides innovative solutions built on advanced technology and in-depth analytics.
+        We focus on delivering high-quality services that empower clients to manage their assets
+        efficiently and achieve their goals with confidence.
+      </p>
+      <p className="mt-4 text-sm">
+        With a blend of expertise and forward-thinking strategies, we ensure reliability and excellence
+        in every aspect. Join us to explore new possibilities and achieve long-term success through collaboration.
+      </p>
+      <div className="mt-4 flex space-x-4">
+    
+      </div>
+    </div>
+
+
+    <div>
+      <h3 className="text-lg font-bold">Company</h3>
+      <ul className="mt-4 space-y-2 text-sm">
+        <li><a href="#" className="hover:underline">About</a></li>
+        <li><a href="#" className="hover:underline">Security</a></li>
+        <li><a href="#" className="hover:underline">Insurance</a></li>
+        <li><a href="#" className="hover:underline">News</a></li>
+        <li><a href="#" className="hover:underline">Support</a></li>
+      </ul>
+    </div>
+
+  
+    <div>
+      <h3 className="text-lg font-bold">Isle of Man <span className="text-sm font-light">(Legal address)</span></h3>
+      <p className="mt-2 text-sm">
+        20 Reayrt Ny Crink, Crosby, IM4 2EA, Isle of Man
+      </p>
+      <a href="mailto:legal@niolic.com" className="block mt-2 text-sm hover:underline">legal@niolic.com</a>
+
+      <h3 className="text-lg font-bold mt-6">United Kingdom <span className="text-sm font-light">(Office)</span></h3>
+      <p className="mt-2 text-sm">
+        30 St. Mary Axe, London, EC3A 8BF, England
+      </p>
+      <a href="mailto:marketing@niolic.com" className="block mt-2 text-sm hover:underline">marketing@niolic.com</a>
+    </div>
+
+ 
+    <div>
+      <h3 className="text-lg font-bold">Contacts</h3>
+      <ul className="mt-4 space-y-2 text-sm">
+        <li><a href="mailto:support@niolic.com" className="hover:underline">support@niolic.com</a></li>
+        <li><a href="mailto:abuse@niolic.com" className="hover:underline">abuse@niolic.com</a></li>
+        <li><a href="mailto:bug.bounty@niolic.com" className="hover:underline">bug.bounty@niolic.com</a></li>
+      </ul>
+      <div className="mt-4 flex space-x-4">
+       
+      </div>
+    </div>
+  </div>
+</footer>
+);
+}
